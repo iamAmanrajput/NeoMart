c6f974c4a5225ec4782d1011c59ef34b7d63cd87
Aman Kumar <aman.it360@gmail.com>
2025-03-02 20:27:35 +0530

    Product Page Completed

--- FRONTEND/src/pages/Product.jsx
+++ FRONTEND/src/pages/Product.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "@/components/custom/Navbar";
 import Footer from "@/components/custom/Footer";
+import { starGenerator } from "@/constants/Helper";
+import { Circle, Minus, Plus } from "lucide-react";
+import { Colors } from "@/constants/colors";
 
 const Product = () => {
+  const [productQuantity, setProductQuantity] = useState(5);
   const imageArray = [
     {
       url: "https://res.cloudinary.com/dmljeib0i/image/upload/v1737486186/products/l8hine0k4jwlw9jmujvz.jpg",
@@ -21,6 +25,9 @@ const Product = () => {
       id: 4,
     },
   ];
+
+  const productStock = 10;
+
   return (
     <div>
       <Navbar></Navbar>
@@ -45,6 +52,88 @@ const Product = () => {
           </div>
 
           {/* right side */}
+          <div className="sm:w-[50%] lg:w-[30%]">
+            <div className="pb-5">
+              <h className="font-extrabold text-2xl">My Keyboard</h>
+              <p className="my-2 text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                pariatur, neque voluptates quam minus repellendus deleniti
+                praesentium minima sed commodi ab molestias, dolore eaque? Et
+                ipsa incidunt odio perferendis!
+              </p>
+              <div className="flex items-center">
+                {starGenerator(4.5, "0", 15)}
+                <span className="text-md ml-1">(2)</span>
+              </div>
+            </div>
+            <div className="py-5 border-t border-b">
+              <h3 className="font-bold text-xl">Rs.500 or Rs.35/month</h3>
+              <p className="text-sm">
+                Suggested payments with 6 months special financing
+              </p>
+            </div>
+            <div className="py-5 border-t border-b">
+              <h3 className="font-bold text-xl">Choose Color</h3>
+              <div className="flex items-center my-2">
+                <Circle
+                  fill={Colors.customIsabelline}
+                  strokeOpacity={0.2}
+                  strokeWidth={0.2}
+                  size={40}
+                />
+                <Circle
+                  fill={Colors.customGray}
+                  strokeOpacity={0.2}
+                  strokeWidth={0.2}
+                  size={40}
+                />
+                <Circle
+                  fill={Colors.customYellow}
+                  strokeOpacity={0.2}
+                  strokeWidth={0.2}
+                  size={40}
+                />
+              </div>
+            </div>
+
+            <div className="py-5">
+              <div className="flex gap-3 items-center">
+                <div className="flex items-center gap-5 bg-gray-100 rounded-full px-3 py-2 w-fit">
+                  <Minus
+                    className="cursor-pointer"
+                    onClick={() =>
+                      setProductQuantity((qty) => (qty > 1 ? qty - 1 : 1))
+                    }
+                    stroke={Colors.customGray}
+                  />
+                  <span className="text-slate-950">{productQuantity}</span>
+                  <Plus
+                    onClick={() =>
+                      setProductQuantity((qty) =>
+                        qty < productStock ? qty + 1 : qty
+                      )
+                    }
+                    className="cursor-pointer"
+                    stroke={Colors.customGray}
+                  />
+                </div>
+                {productStock - productQuantity > 0 && (
+                  <div className="grid text-sm font-semibold text-gray-600">
+                    <span>
+                      Only{" "}
+                      <span className="text-customYellow">
+                        {productStock - productQuantity} items{" "}
+                      </span>
+                      left!
+                    </span>
+                    <span>Don't Miss it</span>
+                  </div>
+                )}
+              </div>
+
+              <div></div>
+            </div>
+          </div>
         </main>
 
         {/* review section */}
