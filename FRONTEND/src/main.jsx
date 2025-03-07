b82e0cb27df7d6e102cb6c2d57494c0c79c55515
Aman Kumar <aman.it360@gmail.com>
2025-03-07 10:11:36 +0530

    Product Slice Added

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -3,11 +3,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/provider/theme-provider";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </ThemeProvider>
 );
