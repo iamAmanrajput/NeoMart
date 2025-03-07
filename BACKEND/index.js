3b220277cc823df85c36086569bc319b57590af9
Aman Kumar <aman.it360@gmail.com>
2025-03-07 10:50:52 +0530

    Backend Files Added

---
+++ BACKEND/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+
+//handling connection error
+app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`<center><h1>Server Running On Port : ${PORT}</h1></center>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port : ${PORT}`);
+});
---
+++ BACKEND/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    otp: {
+      type: Number,
+      default: 0,
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user"],
+    },
+    purchasedProducts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
