f83fd581308882e28f04036f95623fc9ea2a0235
Aman Kumar <aman.it360@gmail.com>
2025-03-11 21:34:39 +0530

    Order Controller Completed

--- BACKEND/controllers/orderController.js
+++ BACKEND/controllers/orderController.js
@@ -104,70 +104,67 @@ exports.updateOrderStatus = async (req, res) => {
   }
 };
 
+//revise this code
 exports.getMetrics = async (req, res) => {
+  if (req.role !== ROLES.admin) {
+    return res.status(403).json({
+      success: false,
+      message: "You are not authorized to access this route",
+    });
+  }
+
+  const { startDate, endDate } = req.query;
+
   try {
-    if (req.role !== ROLES.admin) {
-      return res.status(403).json({
-        success: false,
-        message: "You are not authorized to access this resource",
-      });
-    }
-    const { startDate, endDate } = req.query;
     const start = new Date(
       startDate || new Date().setMonth(new Date().getMonth() - 1)
     );
-
     const end = new Date(endDate || new Date());
 
-    //calculate this month order
+    // Calculate total sales
     const ordersInRange = await Order.find({
-      createdAt: { $gte: start, $lt: end },
+      createdAt: { $gte: start, $lte: end },
     });
-
     const totalSales = ordersInRange.reduce(
-      (acc, order) => acc,
-      order.totalPrice,
+      (acc, order) => acc + order.amount,
       0
     );
 
-    // calculate this month's orders
+    // Calculate this month's orders
     const thisMonthOrders = ordersInRange;
 
-    //get the last month -- agar admin se start,end date de di to nhi milega last month order data
+    // Get the last month
     const lastMonth = new Date(new Date().setMonth(new Date().getMonth() - 1));
 
-    //calculate last month orders
+    // Calculate last month's orders
     const lastMonthOrders = await Order.find({
-      createdAt: { $gte: lastMonth, $lt: start },
+      createdAt: { $gte: lastMonth, $lte: start },
     });
 
-    //calculate total amount of this months orders
+    // Calculate total amount of this month's orders
     const totalThisMonth = thisMonthOrders.reduce(
-      (acc, order) => acc,
-      order.totalPrice,
+      (acc, order) => acc + order.amount,
       0
     );
 
-    //calculate totoal amount of last month's orders
+    // Calculate total amount of last month's orders
     const totalLastMonth = lastMonthOrders.reduce(
-      (acc, order) => acc,
-      order.totalPrice,
+      (acc, order) => acc + order.amount,
       0
     );
 
-    // claculate growth
+    // Calculate growth
     const salesGrowth = totalLastMonth
       ? ((totalThisMonth - totalLastMonth) / totalLastMonth) * 100
       : 0;
 
-    //calculate users
-
+    // Calculate users
     const thisMonthUsers = await User.find({
-      createdAt: { $gte: start, $lt: end },
+      createdAt: { $gte: start, $lte: end },
     });
 
     const lastMonthUsers = await User.find({
-      createdAt: { $gte: lastMonth, $lt: start },
+      createdAt: { $gte: lastMonth, $lte: start },
     });
 
     const usersGrowth = lastMonthUsers.length
@@ -176,10 +173,11 @@ exports.getMetrics = async (req, res) => {
         100
       : 0;
 
-    //calculate users purchased in last hour
+    // Calculate users purchased last hour
     const lastHour = new Date(new Date().setHours(new Date().getHours() - 1));
+
     const lastHourOrders = await Order.find({
-      createdAt: { gte: lastHour, $lte: new Date() },
+      createdAt: { $gte: lastHour, $lte: new Date() },
     });
 
     const previousDayOrders = await Order.find({
@@ -192,31 +190,76 @@ exports.getMetrics = async (req, res) => {
       ? (lastHourOrders.length / previousDayOrders.length) * 100
       : 0;
 
-    // recent sales
+    // Recent sales
     const recentOrders = await Order.find()
       .populate({
         path: "userId",
         select: "name email",
       })
+      .select("amount")
       .sort({ createdAt: -1 })
       .limit(9);
 
-    //product deliverd in last six months with their category and count according to month
+    // products delivered in last 6 months with their category and count according to month
     const sixMonthsAgo = new Date(
       new Date().setMonth(new Date().getMonth() - 6)
     );
-    const sixMonthOrders = await Order.find({
+
+    const sixMonthsOrders = await Order.find({
       createdAt: { $gte: sixMonthsAgo },
     }).populate({
       path: "products.id",
       select: "category",
     });
 
-    return;
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error.message,
+    // get them month wise for eg ; {jan : {keyboard : 1, mouse :1, headset 2}}
+    const monthWise = sixMonthsOrders.reduce((acc, order) => {
+      const month = new Date(order.createdAt).toLocaleString("default", {
+        month: "short",
+      });
+
+      order.products.forEach((product) => {
+        if (!acc[month]) {
+          acc[month] = {};
+        }
+
+        if (!acc[month][product.id.category]) {
+          acc[month][product.id.category] = 1;
+        } else {
+          acc[month][product.id.category]++;
+        }
+      });
+
+      return acc;
+    }, {});
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        totalSales: {
+          count: totalSales,
+          growth: salesGrowth,
+        },
+        users: {
+          count: thisMonthUsers.length,
+          growth: usersGrowth,
+        },
+        sales: {
+          count: totalThisMonth,
+          growth: salesGrowth,
+        },
+        activeNow: {
+          count: lastHourOrders.length,
+          growth: lastHourGrowth,
+        },
+        recentSales: {
+          count: totalThisMonth,
+          users: recentOrders,
+        },
+        sixMonthsBarChartData: monthWise,
+      },
     });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
